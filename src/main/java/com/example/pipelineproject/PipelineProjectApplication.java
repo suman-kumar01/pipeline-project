e40b81cd2df03c2a81570901349537c2af40ca85
suman-kumar01 <samanraj271@gmail.com>
2022-03-12 14:18:38 +0530

    initial commit

---
+++ src/main/java/com/example/pipelineproject/PipelineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipelineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pipelineproject/PipelineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
